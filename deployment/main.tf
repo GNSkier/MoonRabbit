32f864829f0fa68a23fa73cfbfbb52c62e4924fd
GNitta <gtnitta@gmail.com>
2025-11-17 17:28:51 -0800

    "Bigquery Dataset NWS and bq table for terraform"

--- deployment/main.tf
+++ deployment/main.tf
@@ -28,7 +28,7 @@ resource "time_sleep" "wait_for_api_propagation" {
 }
 
 # Storage bucket - only create if explicitly requested
-resource "google_storage_bucket" {
+resource "google_storage_bucket" "data_lake"{
   count         = var.create_gcs_bucket && var.gcs_bucket != "" ? 1 : 0
   name          = var.gcs_bucket
   location      = var.region
@@ -43,8 +43,128 @@ resource "google_storage_bucket" {
   }
 }
 
-resource "NWS_Bigquery_table"{
-    
+resource "google_bigquery_dataset" "NWS_observations"{
+    count = var.bigquery_dataset_NWS && var.create_bigquery_dataset_NWS != "" ? 1  :0
+    dataset_id = var.bigquery_dataset_NWS
+    project = var.project_id
+    location = var.region
 
+    # Explicit dependency to ensure APIs are ready FIRST
+    depends_on = [time_sleep.wait_for_api_propagation]
 
+    labels = {
+        environemnt = "development"
+        managed-by = "terraform"
+    }
 }
+
+resource "google_bigquery_table" "NWS_observations_table"{
+    count = var.bigquery_table_NWS && var.create_bigquery_table_NWS != "" ? 1:0
+    table_id = var.create_bigquery_table_NWS
+    dataset_id = var.bigquery_dataset_NWS
+    project = var.project_id
+
+    schema = jsonencode ([
+        {
+        name = "timestamp"
+        type = "TIMESTAMP"
+        mode = "NULLABLE"
+    },
+    {
+        name = "stationId"
+        type = "STRING"
+        mode = "NULLABLE"
+    },
+    {
+        name = "stationName"
+        type = "STRING"
+        mode = "NULLABLE"
+    },
+    {
+        name = "lat"
+        type = "FLOAT"
+        mode = "NULLABLE"
+    },
+    {
+        name = "lon"
+        type = "FLOAT"
+        mode = "NULLABLE"
+    },
+    {
+        name = "elevation"
+        type = "INTEGER"
+        mode = "NULLABLE"
+    },
+    {
+        name = "temp_unit"
+        type = "STRING"
+        mode = "NULLABLE"
+    },
+    {
+        name = "temp"
+        type = "FLOAT"
+        mode = "NULLABLE"
+    },
+    {
+        name = "pressure_pa"
+        type = "FLOAT"
+        mode = "NULLABLE"
+    },
+    {
+        name = "humidity"
+        type = "FLOAT"
+        mode = "NULLABLE"
+    },
+    {
+        name = "wind_unit"
+        type = "STRING"
+        mode = "NULLABLE"
+    },
+    {
+        name = "wind_speed"
+        type = "FLOAT"
+        mode = "NULLABLE"
+    },
+    {
+        name = "precip_unit"
+        type = "STRING"
+        mode = "NULLABLE"
+    },
+    {
+        name = "precip_3hr"
+        type = "FLOAT"
+        mode = "NULLABLE"
+    },
+    {
+        name = "heat_unit"
+        type = "STRING"
+        mode = "NULLABLE"
+    },
+    {
+        name = "heat_index"
+        type = "FLOAT"
+        mode = "NULLABLE"
+    },
+    {
+        name = "max_temp_24_unit"
+        type = "STRING"
+        mode = "NULLABLE"
+    },
+    {
+        name = "max_temp_24"
+        type = "FLOAT"
+        mode = "NULLABLE"
+    },
+    {
+        name = "min_temp_24_unit"
+        type = "STRING"
+        mode = "NULLABLE"
+    },
+    {
+        name = "min_temp_24"
+        type = "FLOAT"
+        mode = "NULLABLE"
+    }
+        ])
+
+    }
